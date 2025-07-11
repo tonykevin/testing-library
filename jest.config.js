149964a049cc5a93650cc676850cbb9afe99dea0
tonykevin <hi@tonykevin.com>
2025-07-11 16:44:45 -0500

    update jest settings: enable coverage by specific test

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,18 @@ module.exports = {
   },
   collectCoverage: true,
   coverageDirectory: "./coverage",
+  coverageThreshold: {
+    global: {
+      statement: 50,
+      branches: 50,
+      functions: 50,
+      lines: 50
+    },
+    "./src/stats.ts": {
+      statement: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  }
 };
