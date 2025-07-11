2c9f362e5295d251433888347fc5356f463a9d56
tonykevin <hi@tonykevin.com>
2025-07-10 19:50:30 -0500

    add tests: stats - average

--- src/stats.tests.ts
+++ src/stats.tests.ts
@@ -1,7 +1,18 @@
-import {sum} from "./stats";
+import {average, sum} from "./stats";
 
-const result = sum([1, 2,3])
-const expected = 6
+let expected, result
+
+result = sum([1, 2,3])
+expected = 6
+
+if (result === expected) {
+  console.log("✅")
+} else {
+  throw new Error(`❌ ${result} is not equal to ${expected}`)
+}
+
+result = average([1, 2, 3])
+expected = 2
 
 if (result === expected) {
   console.log("✅")
