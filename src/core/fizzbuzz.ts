83cffc13cf3a99bd059bfc11ba14d86d4dc6a4a8
tonykevin <hi@tonykevin.com>
2025-11-20 14:49:31 -0500

    refactor fizzbuzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,8 +1,13 @@
-export function fizzbuzz(number: number) {
-  if(number % 15 === 0)  return 'fizzbuzz'
-  if (number % 3 === 0)  return 'fizz'
-  if (number % 5 === 0)  return 'buzz'
 
+
+export function fizzbuzz(number: number) {
+  function isDivisibleBy(divisor: number): boolean {
+    return number % divisor === 0;
+  }
+
+  if(isDivisibleBy(15))  return 'fizzbuzz'
+  if (isDivisibleBy(3))  return 'fizz'
+  if (isDivisibleBy(5))  return 'buzz'
 
   return number.toString();
 }
